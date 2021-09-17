81d4c220bd7075b903ef05af19ee416a74565273
Salena Echols <salucky9@gmail.com>
2021-09-17 14:35:20 -0400

    added arrays

---
+++ js/index.js
@@ -0,0 +1,47 @@
+//generate a random story thing to pop up when clicked
+var fantasyArray = [
+    'Something',
+    'Another',
+    'All things',
+    'Your Mom',
+    'Sorry for your face',
+    'Alright'
+]
+
+var scifiArray = [
+    'meow',
+    'bats',
+    'big spiders',
+    'tree people',
+    'outer space'
+]
+
+var dramaArray =[
+    'love',
+    'hate',
+    'family',
+    'friends',
+    'animals'
+]
+
+var crimeArray = [
+    'death',
+    'serial killers',
+    'pineapple on pizza',
+    'long pointy nails',
+    'fried crickets',
+    'kicking puppies'
+]
+
+var challengeArray = [
+    'Compliment someone',
+    'Compliment Yourself',
+    'Call a friend (not text)',
+    'Smile at someone',
+    'Help someone without being asked',
+    'Leave the mail person a present in the mailbox'
+]
+
+//make it generate a new story each time it is clicked, randomly
+
+//have a daily personal challenge goal that is random
